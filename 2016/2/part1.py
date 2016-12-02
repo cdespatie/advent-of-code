8e849f35c7e15a12ddfe7662cf9856789c60e73c
Chris Despatie <mail@chrisdespatie.com>
2016-12-02 13:14:50 -0500

    Finished part 2, refactored part 1 some.

--- 2016/2/part1.py
+++ 2016/2/part1.py
@@ -1,17 +1,18 @@
 def main():
-    keypad = ((1,2,3),(4,5,6),(7,8,9)); x = 1; y = 1; ans = []
+    keypad = ((1,2,3),(4,5,6),(7,8,9))
+    x = 1; y = 1; ans = []
     lines = load_input('input-1.txt')
     for line in lines:
         for instr in line:
             if instr == 'R':
-                x = (x + 1) if x < 2 else 2
-            elif instr == 'L':
-                x = (x - 1) if x > 0 else 0
-            elif instr == 'U':
-                y = (y - 1) if y > 0 else 0
-            elif instr == 'D':
                 y = (y + 1) if y < 2 else 2
-        ans.append(keypad[y][x])
+            elif instr == 'L':
+                y = (y - 1) if y > 0 else 0
+            elif instr == 'U':
+                x = (x - 1) if x > 0 else 0
+            elif instr == 'D':
+                x = (x + 1) if x < 2 else 2
+        ans.append(keypad[x][y])
     print(ans)
 
 def load_input(path):
--- 2016/2/part2.py
+++ 2016/2/part2.py
@@ -1,14 +1,22 @@
 def main():
-    keypad = [[1],[2,3,4],[5,6,7,8,9],['A','B','C'],['D']]
-    x = 2; y = 0; ans = []
+    keypad = [[0,0,1,0,0],[0,2,3,4,0],[5,6,7,8,9],[0,'A','B','C',0],[0,0,'D',0,0]]
+    x = 2; y = 0; ans = []; dim = len(keypad) - 1
     lines = load_input('input-1.txt')
     for line in lines:
         for instr in line:
-
-
+            temp_x = x; temp_y = y
+            if instr == 'R':
+                temp_y = (y + 1) if y < dim else dim
+            elif instr == 'L':
+                temp_y = (y - 1) if y > 0 else 0
+            elif instr == 'U':
+                temp_x = (x - 1) if x > 0 else 0
+            elif instr == 'D':
+                temp_x = (x + 1) if x < dim else dim
+            if keypad[temp_x][temp_y] != 0:
+                x = temp_x; y = temp_y
         ans.append(keypad[x][y])
     print(ans)
-
 
 def load_input(path):
     with open(path, 'r') as input_file:
