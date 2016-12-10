2247928a78098522382a7e2aed2bae85d0e60ef8
Chris Despatie <christian_despatie@hotmail.com>
2016-12-09 22:44:13 -0500

    Starting work on d9p1

---
+++ 2016/9/part1.py
@@ -0,0 +1,39 @@
+import re
+
+pattern = r'(\d+)x(\d+)'
+
+def main():
+    data = list(load_input('input-1.txt')[0]); counter = 0
+    while len(data) > 0:
+        char = data.pop(0)
+        if char == '(':
+            code = find_code(data)
+            counter += len(code)
+            digits = list(map(int, re.search(pattern, code).groups()))
+            pop_n(data, digits[0])
+            counter += digits[0] * digits[1]
+        else:
+            counter += 1
+    print(counter)
+
+
+def pop_n(data, n):
+    for i in range(n):
+        data.pop(0)
+
+
+def find_code(data):
+    char = data.pop(0)
+    code = '(' + char
+    while char != ')':
+        char = data.pop(0);
+        code += char
+    return code
+
+def load_input(path):
+    with open(path, 'r') as input_file:
+        data = [line.rstrip('\n') for line in input_file]
+        return data
+
+if __name__ == '__main__':
+    main()
