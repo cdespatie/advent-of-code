f18896b47d879c7042239a72482e4c472be79404
Chris Despatie <mail@chrisdespatie.com>
2017-11-30 22:23:18 -0500

    Experimenting with parsing input into vector for day-0. Good to go.

--- 2017/day-0/src/main.rs
+++ 2017/day-0/src/main.rs
@@ -1,3 +1,23 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("input.txt");
+
+    let mut file = match File::open(&path) {
+        Err(error) => panic!("Error: {}", error.description()),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(_) => panic!(),
+        Ok(_) => print!("{}", s)
+    };
+
+    let sani = s.replace(" ", "");
+    let vec : Vec<&str> = sani.split(",").collect();
+    print!("{:?}", vec);
 }
