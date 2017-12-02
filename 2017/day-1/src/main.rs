41c91416f4d8ae4dab1526ea1799e252fe7020c4
Chris Despatie <mail@chrisdespatie.com>
2017-12-01 22:54:18 -0500

    Cleaning up day 1 code, now that I've remembered how deferencing works.

--- 2017/day-1/src/main.rs
+++ 2017/day-1/src/main.rs
@@ -7,59 +7,38 @@ fn main() {
     let mut file = File::open(&path).unwrap();
 
     let mut s = String::new();
-    file.read_to_string(&mut s);
+    file.read_to_string(&mut s).ok();
 
-    let mut ints : Vec<u32> = s.trim().chars()
+    let ints : Vec<u32> = s.trim().chars()
         .map(|x|
              x.to_digit(10).unwrap()
         ).collect();
 
-    part_1(&mut ints);
-    part_2(&mut ints);
+    part_1(&ints);
+    part_2(&ints);
 }
 
-fn part_2(input: &mut Vec<u32>) {
+fn part_1(input: &Vec<u32>) {
     let mut sum: u32 = 0;
-    let length = input.len();
 
-    for (i, x) in input.iter().enumerate() {
-        let mut index = 0;
-
-        if i + (length / 2) > (length - 1) {
-            index = i - (length / 2);
-        }
-        else {
-            index = (length / 2) + i;
-        }
-
-        if *x == input[index] {
-            sum = &sum + x;
+    for (i, &x) in input.iter().enumerate() {
+        if x == input[(i + 1) % input.len()] {
+            sum += x;
         }
     }
 
-    println!("--- part 2 ---");
-    println!("length: {}", length);
-    println!("sum: {}", sum);
+    println!("part 1: {}", sum);
 }
 
-fn part_1(input: &mut Vec<u32>) {
+fn part_2(input: &Vec<u32>) {
     let mut sum: u32 = 0;
-    let mut prev: u32 = 0;
-    let last: u32 = *input.last().unwrap();
 
-    for x in input.iter() {
-        if x == &prev {
-            sum = &sum + x;
+    for (i, &x) in input.iter().enumerate() {
+        if x == input[(i + input.len() / 2) % input.len()] {
+            sum += x;
         }
-
-        prev = *x;
     }
 
-    if last == input[0] {
-        sum = sum + last;
-    }
-
-    println!("--- part 1 ---");
-    println!("{:?}", sum);
+    println!("part 2: {}", sum);
 }
 
