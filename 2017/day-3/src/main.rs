d0e0dd19e008246a4f31ca783245ecc2af32ba59
Chris Despatie <mail@chrisdespatie.com>
2017-12-04 08:36:09 -0500

    Day 3 pt 2 done, though it's a bit messy - to be cleaned up later. Day 4 cargo init'd.

--- 2017/day-3/src/main.rs
+++ 2017/day-3/src/main.rs
@@ -6,6 +6,9 @@ fn main() {
     let dist = manhat_dist(0, 0, x, y);
     println!("target: {}, {}", x, y);
     println!("distance: {}", dist);
+
+    let ans = get_val(312051);
+    println!("bigger: {}", ans);
 }
 
 fn get_coords(input: u32) -> (i32, i32) {
@@ -26,7 +29,7 @@ fn get_coords(input: u32) -> (i32, i32) {
     (x, y)
 }
 
-fn get_val(input: i32) {
+fn get_val(input: i32) -> i32 {
     let mut dict = HashMap::new();
 
     // Insert base point.
@@ -44,7 +47,46 @@ fn get_val(input: i32) {
 
         x += dx;
         y += dy;
+
+        let value = calc_val(x, y, &dict);
+        dict.insert(Point::new(x, y), value);
+
+        if value > input {
+            return value;
+        }
     }
+    0
+}
+
+fn calc_val(x: i32, y: i32, hash: &HashMap<Point, i32>) -> i32 {
+    let mut sum: i32 = 0;
+    let mut point: Point;
+
+    point = Point::new(x + 1, y);
+    if hash.contains_key(&point) { sum += hash.get(&point).unwrap() }
+
+    point = Point::new(x + 1, y + 1);
+    if hash.contains_key(&point) { sum += hash.get(&point).unwrap() }
+
+    point = Point::new(x, y + 1);
+    if hash.contains_key(&point) { sum += hash.get(&point).unwrap() }
+
+    point = Point::new(x - 1, y);
+    if hash.contains_key(&point) { sum += hash.get(&point).unwrap() }
+
+    point = Point::new(x - 1, y - 1);
+    if hash.contains_key(&point) { sum += hash.get(&point).unwrap() }
+
+    point = Point::new(x, y - 1);
+    if hash.contains_key(&point) { sum += hash.get(&point).unwrap() }
+
+    point = Point::new(x + 1, y - 1);
+    if hash.contains_key(&point) { sum += hash.get(&point).unwrap() }
+
+    point = Point::new(x - 1, y + 1);
+    if hash.contains_key(&point) { sum += hash.get(&point).unwrap() }
+
+    sum
 }
 
 fn manhat_dist(x_0: i32, y_0: i32, x_1: i32, y_1: i32) -> i32 {
