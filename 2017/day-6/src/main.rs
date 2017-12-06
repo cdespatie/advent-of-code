7008c294e59e23b9aa1644a7be7822280f7aed2e
Chris Despatie <mail@chrisdespatie.com>
2017-12-06 13:45:43 -0500

    Did day 6. Also went back and changed some things to be more idiomatic after reading some other Rust solutions. More functional!

--- 2017/day-6/src/main.rs
+++ 2017/day-6/src/main.rs
@@ -1,15 +1,45 @@
+use std::collections::HashSet;
+
 fn main() {
     let input = include_str!("../input.txt");
     let parsed_input: Vec<u32> = input.trim().split_whitespace()
         .map(|x| x.parse::<u32>().unwrap()).collect();
 
-    println!("{:?}", parsed_input);
-    println!("{}", convert(&parsed_input));
+    println!("Input: {:?}", parsed_input);
+    reallocate(&mut parsed_input.clone());
 }
 
-fn part_1(input: &Vec<u32>) {
+fn reallocate(input: &mut Vec<u32>) {
+    let input_size = input.len();
+    let mut hash: HashSet<Vec<_>> = HashSet::new();
+    let mut toggle = false;
+    let mut p1_counter = 0;
+    let mut p2_counter = 0;
+
+    loop {
+        hash.insert(input.clone());
+        let (i, &val) = input.iter().enumerate()
+            .max_by_key(|&(i, val)| (val, -(i as isize))).unwrap();
+
+        input[i] = 0;
+        for j in 0..val as usize {
+            input[(i + j + 1) % input_size] += 1;
+        }
+
+        match toggle {
+            true => p2_counter += 1,
+            _    => p1_counter += 1
+        };
+
+        if hash.contains(input) {
+            if toggle {
+                println!("p1 - #iters: {}\np2 - #iters: {}", p1_counter, p2_counter);
+                break;
+            }
+
+            hash = HashSet::new();
+            toggle = true;
+        }
+    }
 }
 
-fn convert(input: &Vec<u32>) -> String {
-    input.iter().map(|x| x.to_string()).collect::<Vec<String>>().connect(",")
-}
