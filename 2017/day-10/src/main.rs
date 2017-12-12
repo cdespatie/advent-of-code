d6928030bf86282089a78a557b54f45305130aaa
Chris Despatie <mail@chrisdespatie.com>
2017-12-12 08:38:32 -0500

    Working on day 10 p2. Code done, bugs somewhere though.

--- 2017/day-10/src/main.rs
+++ 2017/day-10/src/main.rs
@@ -1,9 +1,61 @@
 fn main() {
     let input = include_str!("../input.txt").trim();
-    solve(input);
+    let mut const_chars: Vec<usize> = vec![17, 31, 73, 47, 23];
+
+    let mut parsed = Vec::new();
+    for i in input.chars() {
+        parsed.push(i as usize);
+    }
+    println!("{:?}", parsed);
+    parsed.append(&mut const_chars);
+    println!("{:?}", parsed);
+
+    let ascii_chars: String = parsed.iter().map(|x| x.to_string())
+        .collect::<Vec<String>>().join(",");
+
+    knot_hash(ascii_chars, 64);
 }
 
-fn solve(input: &str) {
+fn knot_hash(input: String, reps: usize) {
+    let mut start: usize = 0;
+    let mut skip: usize = 0;
+    let mut vector: Vec<usize> = Vec::new();
+
+    for i in 0..256 { vector.push(i); }
+
+    for rep in 0..reps {
+        for n in input.split(",") {
+            let split = get_split(&vector, start, n.parse::<usize>().unwrap());
+
+            for (i, &x) in split.iter().rev().enumerate() {
+                vector[(start + i) % 256] = x;
+            }
+
+            start = (start + split.len() + skip) % 256;
+            skip += 1;
+        }
+    }
+
+    let mut dense = Vec::new();
+
+    for i in 0..16 {
+        let mut temp = 0;
+        for j in 0..16 {
+            temp = temp ^ vector[(i * 16) + j];
+        }
+
+        dense.push(temp);
+    }
+
+    println!("{:?}", dense);
+    println!("{:?}", convert_hex(dense));
+}
+
+fn convert_hex(input: Vec<usize>) -> String {
+    input.iter().map(|x| format!("{:x}", x)).collect::<Vec<_>>().join("")
+}
+
+fn part_1(input: &str) {
     let mut vector: Vec<usize> = Vec::new();
     let mut start: usize = 0;
     for i in 0..256 { vector.push(i); }
