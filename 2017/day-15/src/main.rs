7246ecee7c743a65ca8aa40713fef8c4972ffd5f
Chris Despatie <mail@chrisdespatie.com>
2017-12-28 09:54:11 -0500

    Day 15 done.

--- 2017/day-15/src/main.rs
+++ 2017/day-15/src/main.rs
@@ -1,18 +1,51 @@
 fn main() {
     let mut gen_a = 634;
     let mut gen_b = 301;
-    let mut counter = 0;
+    let mut counter_p1 = 0;
+    let mut counter_p2 = 0;
 
+    // Part 1
     for _ in 0..40000000 {
         gen_a = generator(gen_a, 16807);
         gen_b = generator(gen_b, 48271);
 
         if (gen_a & 0xFFFF) == (gen_b & 0xFFFF) {
-            counter += 1;
+            counter_p1 += 1;
         }
     }
 
-    println!("{}", counter);
+    // Part 2
+    let mut vec_a: Vec<u32> = Vec::new();
+    let mut vec_b: Vec<u32> = Vec::new();
+    gen_a = 634;
+    gen_b = 301;
+
+    while vec_a.len() < 5000000 {
+        gen_a = generator(gen_a, 16807);
+
+        if gen_a % 4 == 0 {
+            vec_a.push(gen_a);
+        }
+    }
+
+    while vec_b.len() < 5000000 {
+        gen_b = generator(gen_b, 48271);
+
+        if gen_b % 8 == 0 {
+            vec_b.push(gen_b);
+        }
+    }
+
+    for (i, a) in vec_a.iter().enumerate() {
+        let b = vec_b[i];
+
+        if (a & 0xFFFF) == (b & 0xFFFF) {
+            counter_p2 += 1;
+        }
+    }
+
+    println!("{}", counter_p1);
+    println!("{}", counter_p2);
 }
 
 fn generator(value: u32, factor: u32) -> u32 {
