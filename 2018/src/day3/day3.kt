6a80161989e67b08111f1e23b9b033621669c4d9
Chris Despatie <mail@chrisdespatie.com>
2018-12-05 08:47:19 -0500

    Part 1 done, part 2 has bug.

--- 2018/src/day3/day3.kt
+++ 2018/src/day3/day3.kt
@@ -6,24 +6,36 @@ fun day3() {
     val inputLines = File("./src/day3/input.txt").readLines()
 
     val parsed = inputLines.map { parse(it) }
-    part1(parsed)
+    bothParts(parsed)
 }
 
-fun part1(instructions: Collection<Instruction>) {
+fun bothParts(instructions: Collection<Instruction>) {
     val array = Array(1001) { Array(1001) { "-" } }
 
     for (instruction in instructions) {
-        for (i in instruction.x..(instruction.x + instruction.width)) {
-            for (j in instruction.y..(instruction.y + instruction.height)) {
-                array[i][j] = if (array[i][j] != "-") "#" else "x"
+        for (i in instruction.x until (instruction.x + instruction.width)) {
+            for (j in instruction.y until (instruction.y + instruction.height)) {
+                if (array[i][j] != "-" && array[i][j] != "#") {
+                    markInstructionIncomplete(instructions, array[i][j])
+                    array[i][j] = "#"
+                }
+                else {
+                    array[i][j] = instruction.id.toString()
+                }
             }
         }
     }
 
     val overlaps = array.map { x -> x.count { y -> y == "#"} }.sum()
+    val complete = instructions.filter { x -> x.isComplete }
 
-//    printMap(array)
     println(overlaps)
+    println(complete)
+}
+
+fun markInstructionIncomplete(instructions: Collection<Instruction>, id: String) {
+    val instruction = instructions.find { x -> x.id == id.toInt() }
+    instruction?.isComplete = false
 }
 
 fun parse(input: String): Instruction {
@@ -45,4 +57,6 @@ fun printMap(array: Array<Array<String>>) {
     }
 }
 
-data class Instruction(val id: Int, val x: Int, val y: Int, val width: Int, val height: Int)
+data class Instruction(val id: Int, val x: Int, val y: Int, val width: Int, val height: Int) {
+    var isComplete: Boolean = true
+}
