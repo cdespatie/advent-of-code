2397b4a872ad8b3d3c9d49615d8f8cc0dc87a9c6
Chris Despatie <mail@chrisdespatie.com>
2018-12-05 08:28:40 -0500

    Continuing work on day 3. Count is wrong for some reason.

--- 2018/src/day3/day3.kt
+++ 2018/src/day3/day3.kt
@@ -5,9 +5,25 @@ import java.io.File
 fun day3() {
     val inputLines = File("./src/day3/input.txt").readLines()
 
-    for (line in inputLines) {
-        println(parse(line))
+    val parsed = inputLines.map { parse(it) }
+    part1(parsed)
+}
+
+fun part1(instructions: Collection<Instruction>) {
+    val array = Array(1001) { Array(1001) { "-" } }
+
+    for (instruction in instructions) {
+        for (i in instruction.x..(instruction.x + instruction.width)) {
+            for (j in instruction.y..(instruction.y + instruction.height)) {
+                array[i][j] = if (array[i][j] != "-") "#" else "x"
+            }
+        }
     }
+
+    val overlaps = array.map { x -> x.count { y -> y == "#"} }.sum()
+
+//    printMap(array)
+    println(overlaps)
 }
 
 fun parse(input: String): Instruction {
@@ -18,6 +34,15 @@ fun parse(input: String): Instruction {
     return Instruction(groupVals?.get(1)?.toInt() ?: 0, groupVals?.get(2)?.toInt() ?: 0,
     groupVals?.get(3)?.toInt() ?: 0, groupVals?.get(4)?.toInt() ?: 0,
         groupVals?.get(5)?.toInt() ?: 0)
+}
+
+fun printMap(array: Array<Array<String>>) {
+    for (x in array) {
+        for (y in x) {
+            print(y)
+        }
+        println("")
+    }
 }
 
 data class Instruction(val id: Int, val x: Int, val y: Int, val width: Int, val height: Int)
